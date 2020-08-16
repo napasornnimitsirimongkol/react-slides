d7da9139f03d2e410c210b2168c869e89597af3b
luar56273 <luar56273@gmail.com>
2020-08-16 12:40:02 +0700

    first commit

---
+++ src/componets/Header.js
@@ -0,0 +1,64 @@
+/*
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <Link to="/">หน้าหลัก</Link>
+                <Link to="About/">เกี่ยวกับเรา</Link>
+                <Link to="Product/">สินค้า</Link>
+                <Link to="Contact/">ติดต่อเรา</Link>
+                <Link to="Profile/">ข้อมูลส่วนตัว</Link>
+            </div>
+        )
+    }
+} 
+*/
+
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  
+} from 'reactstrap';
+
+const Header = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand tag={Link}to="/">napasorn</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink tag={Link}to="/about">เกี่ยวกับเรา</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link}to="/contact">ติดต่อเรา</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link}to="/prodoct">สินค้า</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link}to="/profile">ข้อมูลส่วนตัว</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/componets/Product.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Product extends Component {
+    render() {
+        return (
+            <div>
+                <h2>สินค้า</h2>
+            </div>
+        )
+    }
+}
