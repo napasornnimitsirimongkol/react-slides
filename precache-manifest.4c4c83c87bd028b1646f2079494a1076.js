e27b70693d469631e1e6c0565d18e242389586d9
zboompopz <zboompopz@hotmail.com>
2020-08-30 12:07:23 +0700

    Updates

---
+++ precache-manifest.4c4c83c87bd028b1646f2079494a1076.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf1be6c399341ac909cd2db9a458712a",
+    "url": "/businessweb/index.html"
+  },
+  {
+    "revision": "8fff834bbb229ae27f98",
+    "url": "/businessweb/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "2b5999ca94febfeeb89f",
+    "url": "/businessweb/static/css/main.1151150e.chunk.css"
+  },
+  {
+    "revision": "8fff834bbb229ae27f98",
+    "url": "/businessweb/static/js/2.57d113bc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/businessweb/static/js/2.57d113bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b5999ca94febfeeb89f",
+    "url": "/businessweb/static/js/main.90d97339.chunk.js"
+  },
+  {
+    "revision": "da965bd051eb320fbc77",
+    "url": "/businessweb/static/js/runtime-main.b7b492d2.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/businessweb/static/media/logo.ee7cd8ed.svg"
+  }
+]);
